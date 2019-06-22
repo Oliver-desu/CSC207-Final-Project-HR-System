2216d5ac3ae32f4b4a42038a666fa73b19dce3cb
Sharon <fangchu.lin@mail.utoronto.ca>
2019-06-22 16:54:28 -0400

    Rename application class

--- phase1/src/Application.java
+++ phase1/src/Application.java
@@ -1,57 +1,47 @@
 import java.time.LocalDate;
+public class Application {
 
-import java.util.Date;
-import java.util.List;
+    private JobPosting jobposting;
+    private Applicant applicant;
+    private LocalDate closeDate;
+    private String status;
+    private Document douments;
 
-public class Applicant extends  User{
-    private String username;
-    private String password;
-    private List<Document> Documents;
-    private List<Applicantion> current_Applications;
-    private List<Applicantion> past_Applications;
-    private List<Applicantion> all_Applications;
-    private static List<interview> interviews;
-    private LocalDate dateCreated;
-    private LocalDate lastAppliedDate;
-
-
-    public Applicant(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-    public String getUsername() {
-        return this.username;
+    public Application(JobPosting jobposting, Applicant applicant) {
+        this.jobposting = jobposting;
+        this.applicant = applicant;
     }
 
-    public String getPassword() {
-        return this.password;
+    public void setStatus(String status) {
+        this.status = status;
     }
 
-    public void setPassword(String password) {
-        this.password = password;
+    public void setCloseDate() {
+
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setDocument(Document documents) {
+        this.documents = documents;
     }
 
-    public boolean matchPassword(String password) {
-        return this.password.equals(password);
+    public JobPosting getJobPosting() {
+        return this.jobposting;
     }
 
-    public List<Document> get_documents(){return  this.Documents}
+    public ArrayList<Document> getDocument(){
+        return this.documents;
+    }
 
-    public List<Applicantion> getCurrent_Applications(){return  this.current_Applications;}
+    public String shortForm() {
 
-    public List<Applicantion> getPast_Applications(){return  this.past_Applications;}
+    }
 
-    public  List<Applicantion> getAll_Applications(){return  this.all_Applications;}
+    public String detailForm() {
 
-    public void addInterviews(Interview a){interviews.add(a);}
+    }
+
+
 
 
 }
-
-
-
 
