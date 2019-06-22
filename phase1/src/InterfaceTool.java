a0cb77bf88efc16445eb9de226f434ff3f5ebe9d
cheny402 <oliveryi.chen@mail.utoronto.ca>
2019-06-22 15:50:26 -0400

    Create interface tool and set up basic settings for easier setup in the future

---
+++ phase1/src/InterfaceTool.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionListener;
+
+public class InterfaceTool {
+    static JFrame createInterface(String title, int width, int height){
+        // create interface with desired setting
+        JFrame newInterface = new JFrame(title);
+        newInterface.setSize(width,height);
+        newInterface.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);// exit code when close window
+        newInterface.setLocationRelativeTo(null);// window pop in the middle of screen
+        newInterface.setResizable(false); // window unstretchable
+
+        // flow layout
+        int gapWidth = width/20;
+        int gapHeight = gapWidth/4;
+        FlowLayout layout=new FlowLayout(FlowLayout.CENTER,gapWidth,gapHeight);
+        newInterface.setLayout(layout);
+        return newInterface;
+    }
+
+    private static void setDimension(JComponent jComponent, int width, int height){
+        jComponent.setPreferredSize(new Dimension(width, height));
+    }
+
+    static JLabel createTextLabel(String text, int width, int height){
+        JLabel label=new JLabel(text);
+        setDimension(label, width, height);
+        return label;
+    }
+    static JLabel createTextLabel(String text){
+        return new JLabel(text);
+    }
+
+    static JTextField createTextField(int width, int height){
+        JTextField textField = new JTextField();
+        setDimension(textField, width, height);
+        return textField;
+    }
+
+    static JPasswordField createPasswordField(int width, int height){
+        JPasswordField jPasswordField = new JPasswordField();
+        setDimension(jPasswordField,width, height);
+        return jPasswordField;
+    }
+
+    static JButton createButton(String text, int width, int height, ActionListener actionListener){
+        JButton jButton = new JButton(text);
+        setDimension(jButton, width, height);
+        jButton.addActionListener(actionListener);
+        return jButton;
+    }
+
+    static JCheckBox createCheckBox(int size){
+        JCheckBox checkBox = new JCheckBox();
+        setDimension(checkBox, size, size);
+        return checkBox;
+    }
+
+    static void addUserInterface(JFrame jFrame, ActionListener account, ActionListener password, ActionListener logout){
+        int width = jFrame.getWidth()/4;
+        int height = (int)(width*0.4);
+        jFrame.add(createButton("Account", width, height, account));
+        jFrame.add(createButton("Edit Password", width, height, password));
+        jFrame.add(createButton("Logout", width, height, logout));
+    }
+}
+
