c1a0765a8885b705624ad02c27f7c5888cd09360
Kerwyny <40316624+Kerwyny@users.noreply.github.com>
2019-06-22 16:51:41 -0400

    Create Company, HumanResource, Interview, and Interviewer; add basic getter and setter methods, and some unfinished methods that will be implemented and used in the future

---
+++ phase1/src/Interview.java
@@ -0,0 +1,77 @@
+import java.time.LocalDate;
+
+public class Interview {
+
+    private enum Status {
+        PASS,
+        REJECT,
+        PENDING
+    }
+
+    private LocalDate date;
+    private JobPosting jobPosting;
+    private Interviewer interviewer;
+    private Application application;
+    private String round;
+    private Status status = Status.PENDING;
+    private String recommandation;
+
+    public Interview(LocalDate date, JobPosting jobPosting, Interviewer interviewer, Application application,
+                     String round) {
+        this.date = date;
+        this.jobPosting = jobPosting;
+        this.interviewer = interviewer;
+        this.application = application;
+        this.round = round;
+    }
+
+    public LocalDate getDate() {
+        return this.date;
+    }
+
+    public JobPosting getJobPosting() {
+        return this.jobPosting;
+    }
+
+    public Interviewer getInterviewer() {
+        return this.interviewer;
+    }
+
+    public Application getApplication() {
+        return this.application;
+    }
+
+    public String getRound() {
+        return this.round;
+    }
+
+    public Status getStatus() {
+        return this.status;
+    }
+
+    public String getRecommandation() {
+        return this.recommandation;
+    }
+
+    public void setRecommandation(String recommandation) {
+        this.recommandation = recommandation;
+    }
+
+//    Unfinished methods:
+
+    public String toStringForApplicant() {
+        return null;
+    }
+
+    public String toStringForStaff() {
+        return null;
+    }
+
+    public void passInterview() {
+
+    }
+
+    public void failInterview() {
+
+    }
+}
