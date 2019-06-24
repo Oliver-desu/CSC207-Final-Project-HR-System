4359b326dfe61b1892277cd730f4e1812b0e0c2b
baibizhe <bbzatyddounan@gmail.com>
2019-06-24 00:57:41 -0400

    implement Applicant Document

--- phase1/src/Applicant.java
+++ phase1/src/Applicant.java
@@ -3,17 +3,20 @@ import login.SearchObject;
 import java.awt.event.ActionListener;
 import java.time.LocalDate;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 public class Applicant extends  User implements SearchObject {
     private String username;
     private String password;
-    private List<Document> Documents;
-    private List<Application> current_Applications;
-    private List<Application> past_Applications;
-    private List<Application> all_Applications;
-    private static List<Interview> interviews;
+    private List<Document> documents =new ArrayList<>();
+    private List<Application> current_Applications = new ArrayList<>();
+    private List<Application> past_Applications =new ArrayList<>();
+    private List<Application> all_Applications =new ArrayList<>();
+    private  List<Interview> interviews =new ArrayList<>();
+    private static List<Applicant>allApplicants =new ArrayList<>();
     private LocalDate dateCreated;
     private LocalDate lastAppliedDate;
 
@@ -21,6 +24,7 @@ public class Applicant extends  User implements SearchObject {
     public Applicant(String username, String password) {
         this.username = username;
         this.password = password;
+
     }
     public String getUsername() {
         return this.username;
@@ -42,7 +46,7 @@ public class Applicant extends  User implements SearchObject {
         return this.password.equals(password);
     }
 
-    public List<Document> get_documents(){return  this.Documents;}
+    public List<Document> get_documents(){return  this.documents;}
 
     public List<Application> getCurrent_Applications(){return  this.current_Applications;}
 
@@ -51,6 +55,65 @@ public class Applicant extends  User implements SearchObject {
     public  List<Application> getAll_Applications(){return  this.all_Applications;}
 
     public void addInterviews(Interview a){interviews.add(a);}
+
+    public List<Interview> getInterviews(){return  interviews;}
+
+    public List<JobPosting> viewJobs(ArrayList<JobPosting> a){return a;}
+
+    public  Application createApplication(JobPosting a){
+        Application b = new Application(a,this);
+        this.all_Applications.add(b);
+        return  b;
+        //creat application with jobposing and return it
+
+    }
+    public  void withdrawApplication(Application a){
+        a.setApplicant(null);
+        a.setJobposting(null);
+        // set Applicant and Jobposting i order to withdraw
+    }
+
+    public  void submitDocument(Document d){
+        this.documents.add(d);
+    }
+
+    public  boolean applicant_already_created(String username){
+        for (Applicant a:allApplicants
+        ) {if(a.getUsername().equals(username)){return true;}
+
+        }
+        return false;
+    }//to determine whether there is a user already
+
+    public Object get_application(String username){
+        for (Applicant a:allApplicants
+             ) {if(a.getUsername().equals(username)){return a.getAll_Applications();}
+
+        }
+        return "No such User";
+
+    }
+
+    public List<Document> getDocument(){return  this.documents;}
+    public  Object getCV(){
+        for (Document a:documents
+        ) {if(a.get_CV_or_not()){return a;}
+
+        }
+        Document d = new Document();
+        return d;
+    }
+
+    public Document  getCoverLetter(){
+        for (Document a:documents
+        ) {if(!a.get_CV_or_not()){return a;}
+
+        }
+        Document d = new Document();
+        return d;
+    }
+
+
 
     @Override
     public ActionListener getSelectAction() {
--- phase1/src/Document.java
+++ phase1/src/Document.java
@@ -3,8 +3,20 @@ import login.SearchObject;
 import java.awt.event.ActionListener;
 
 public class Document implements SearchObject {
+    private  boolean CV_or_not;//if document is CV then this will be true
+    private  String content;
     public  Document(){System.out.println("this is document");}
 
+    public void update_document(String a){
+        this.content = a;
+    }
+
+    public  void add_words(String a){
+        this.content+=a;
+    }
+
+    public String getContent(){return this.content;}
+    public  boolean get_CV_or_not(){return CV_or_not;}
     @Override
     public ActionListener getSelectAction() {
         return null;
@@ -23,5 +35,10 @@ public class Document implements SearchObject {
     @Override
     public String getSearchValue1() {
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
     }
 }
