9749bf3ffab780143c6573fd3dbdd4251e7ab007
Sharon <fangchu.lin@mail.utoronto.ca>
2019-06-28 14:32:43 -0400

    Modified and added get status method

--- phase1/src/domain/Application.java
+++ phase1/src/domain/Application.java
@@ -31,6 +31,10 @@ public class Application implements SearchObject {
         this.status = status;
     }
 
+    public String getStatus(){
+        return this.status;
+    }
+
     public void setCloseDate() {
 
     }
