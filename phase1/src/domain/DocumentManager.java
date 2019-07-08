37418424b658c4bc2de80441daab7472ef9eadd8
Yichun Zhang <yichun.zhang@mail.utoronto.ca>
2019-07-08 17:19:25 -0400

    Add four classes: AccountManager, DocumentManager, JobPostingManager and IOStream. Implement some simple methods in them.

---
+++ phase1/src/domain/DocumentManager.java
@@ -0,0 +1,28 @@
+package domain;
+
+class DocumentManager{
+
+
+
+    void viewDocument(){
+//        todo
+    }
+
+    void createNewDocument(){
+//        todo
+    }
+
+    boolean deleteDocument(){
+//        todo
+        return false;
+    }
+
+    void updateDocument(){
+//        todo
+    }
+    boolean deleteAllFiles(){
+//        todo
+        return false;
+    }
+
+}
