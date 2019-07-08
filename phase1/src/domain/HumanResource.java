21d1084c4b7a2f92c08e8bdae3bbb95f0e1e914a
Kerwyny <40316624+Kerwyny@users.noreply.github.com>
2019-07-08 13:41:16 -0400

    Update HumanResource class

--- phase1/src/domain/HumanResource.java
+++ phase1/src/domain/HumanResource.java
@@ -4,55 +4,122 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class HumanResource extends User {
+public class HumanResource {
 
-    private static ArrayList<HumanResource> allHumanResources = new ArrayList<HumanResource>();
+    private HashMap<String, ArrayList<JobPosting>> responsibleFor;
     private Company company;
 
-    public HumanResource(String username, String password, LocalDate dateCreated, Company company) {
-        super(username, password, dateCreated);
+    HumanResource(Company company) {
         this.company = company;
-        allHumanResources.add(this);
+        this.responsibleFor = new HashMap<>();
+        this.responsibleFor.put("open", new ArrayList<>());
+        this.responsibleFor.put("interviewing", new ArrayList<>());
+        this.responsibleFor.put("waiting for next round", new ArrayList<>());
+        this.responsibleFor.put("filled", new ArrayList<>());
+        this.responsibleFor.put("unfilled", new ArrayList<>());
     }
 
-    public static ArrayList<HumanResource> getAllHumanResources() {
-        return allHumanResources;
-    }
-
-    public Company getCompany() {
-        return this.company;
-    }
-
-
-    public ArrayList<Applicant> getApplicants() {
-        return this.company.getApplicants();
-    }
-
-    public ArrayList<JobPosting> getJobPostings(){
-        return this.company.getJobPostings();
-    }
-
-    public HashMap<String, String> getAccount() {
-        HashMap<String, String> result = ((User) this).getAccount();
-        result.put("Company name", this.company.getCompanyName());
-        return result;
-    }
-
-    public Interviewer getInterviewer(String realName){
-        ArrayList<Interviewer> interviewers = this.company.getInterviewers();
-        for (Interviewer interviewer: interviewers) {
-            if (realName.equals(interviewer.getRealName())) {
-                return interviewer;
-            }
+    String postJob(ArrayList<Object> objects) {
+        String message;
+        if (objects.size() < 4) {
+            message = "Please fill all fields.";
+        } else {
+             JobPosting jobPosting = new JobPosting(objects);
+             responsibleFor.get(jobPosting.getCurrentState()).add(jobPosting);
+             message = "Job posted.";
         }
-        return null;
+        return message;
     }
 
-    @Override
-    public String toString() {
-        return ((User) this).toString() + "\n" +
-                "Company name: " + this.company.getCompanyName();
+    void removePost(JobPosting jobPosting) {
+        for (ArrayList<JobPosting> jobPostings: this.responsibleFor.values()) {
+            jobPostings.remove(jobPosting);
+        }
     }
 
+    String matchInterview(JobPosting jobPosting, Application application, Interviewer interviewer, LocalDate date) {
+        Interview interview = new Interview(date, jobPosting, interviewer, application.getApplicant());
+        interviewer.addInterview(interview);
+        application.addInterview(interview);
+        application.setState("interviewing");
+        String message;
+        if (jobPosting.getCurrentState().equals("waitingForNextRound")) {
+            jobPosting.fromWaitingForRound();
+            message = "interview created";
+        } else {
+            message = "can't match interview at this stage";
+        }
+        return message;
+    }
+
+    void hire(JobPosting jobPosting, Applicant applicant) {
+        jobPosting.removeApplicant(applicant);
+        jobPosting.addApplicant(applicant, "hired");
+    }
+
+    void reject(JobPosting jobPosting, Applicant applicant) {
+        jobPosting.removeApplicant(applicant);
+        jobPosting.addApplicant(applicant, "rejected");
+    }
 
 }
+
+
+
+
+
+
+
+
+//public class HumanResource extends User {
+
+//    private static ArrayList<HumanResource> allHumanResources = new ArrayList<HumanResource>();
+//    private Company company;
+//
+//    public HumanResource(String username, String password, LocalDate dateCreated, Company company) {
+//        super(username, password, dateCreated);
+//        this.company = company;
+//        allHumanResources.add(this);
+//    }
+//
+//    public static ArrayList<HumanResource> getAllHumanResources() {
+//        return allHumanResources;
+//    }
+//
+//    public Company getCompany() {
+//        return this.company;
+//    }
+//
+//
+//    public ArrayList<Applicant> getApplicants() {
+//        return this.company.getApplicants();
+//    }
+//
+//    public ArrayList<JobPosting> getJobPostings(){
+//        return this.company.getJobPostings();
+//    }
+//
+//    public HashMap<String, String> getAccount() {
+//        HashMap<String, String> result = ((User) this).getAccount();
+//        result.put("Company name", this.company.getCompanyName());
+//        return result;
+//    }
+//
+//    public Interviewer getInterviewer(String realName){
+//        ArrayList<Interviewer> interviewers = this.company.getInterviewers();
+//        for (Interviewer interviewer: interviewers) {
+//            if (realName.equals(interviewer.getRealName())) {
+//                return interviewer;
+//            }
+//        }
+//        return null;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return ((User) this).toString() + "\n" +
+//                "Company name: " + this.company.getCompanyName();
+//    }
+//
+//
+//}
