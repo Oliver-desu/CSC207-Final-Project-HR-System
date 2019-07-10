be7c027b3074d4b099d5707aae915f3f9da0126e
baibizhe <bbzatyddounan@gmail.com>
2019-07-10 00:52:30 -0400

    update Application.java and company.java

--- phase1/src/domain/Company.java
+++ phase1/src/domain/Company.java
@@ -1,19 +1,20 @@
 package domain;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Company extends  User{
     private  String companyName;
     private  HashMap<String,ArrayList<JobPosting>> JobPostings;
-    private  HumanResource hr;
-    private  HashMap<String,ArrayList<JobPosting>> JobPostingByState;
-    // i will try to add limitation on this attributye
+    // i will try to add limitation on this attribute
     ////Key: "open", "interviewing", "waiting for next round", "pending", "filled", "unfilled"
 
     private  HashMap<String,ArrayList<JobPosting>> interviewers;
-    private  HashMap<String,ArrayList<JobPosting>> hrCoordinator;
 
+    private  HashMap<String,ArrayList<Interview>> upcomingInterview;
+    private  HashMap<String,ArrayList<Interview>> waitingForResultInterviews;
 
 
     public  Company(String username,String password){
@@ -21,35 +22,35 @@ public class Company extends  User{
     }
 
     //getters
-    public HashMap<String,ArrayList<JobPosting>> getJobPostingByState(){
-        return  JobPostingByState;
-    }
 
     public HashMap<String, ArrayList<JobPosting>> getJobPostings() {
         return JobPostings;
     }
 
-    public HashMap<String, ArrayList<JobPosting>> getHrCoordinator() {
-        return hrCoordinator;
+    public HashMap<String, ArrayList<Interview>> getUpcomingInterview() {
+        return upcomingInterview;
+    }
+
+    public HashMap<String, ArrayList<Interview>> getWaitingForResultInterviews() {
+        return waitingForResultInterviews;
     }
 
     public String getCompanyName() { return companyName; }
 
-    public HumanResource getHr() {
-        return hr;
-    }
 
     public HashMap<String,ArrayList<JobPosting>> getInterviewers() {
         return interviewers;    }
 
-    public ArrayList<JobPosting> getJobPostings(JobPostingState key) {
-        return this.JobPostings.get(key);
-    }
 
     //setters
 
-    public void setJobPostingByState(HashMap<String, ArrayList<JobPosting>> jobPostingByState) {
-        JobPostingByState = jobPostingByState;
+
+    public void setWaitingForResultInterviews(HashMap<String, ArrayList<Interview>> waitingForResultInterviews) {
+        this.waitingForResultInterviews = waitingForResultInterviews;
+    }
+
+    public void setUpcomingInterview(HashMap<String, ArrayList<Interview>> upcomingInterview) {
+        this.upcomingInterview = upcomingInterview;
     }
 
     public void setInterviewers(HashMap<String, ArrayList<JobPosting>> interviewers) {
@@ -57,35 +58,81 @@ public class Company extends  User{
     }
 
     public void setJobPostings(HashMap<String, ArrayList<JobPosting>> jobPostings) {
-        JobPostings = jobPostings;
-    }
+        JobPostings = jobPostings;   }
 
-    public void setHrCoordinator(HashMap<String, ArrayList<JobPosting>> hrCoordinator) {
-        this.hrCoordinator = hrCoordinator;
-    }
 
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
 
-    public void setHr(HumanResource hr) {
-        this.hr = hr;
-    }
-
-
-
 
     // end ---------------------------
+    public  String assignInterview(String interviewer ,  Application application, LocalDate date){
+        try {
+            Interview interview = new Interview(interviewer,application,date);
 
-    public  void removeHRCoords(User user){
-        // need to be done
+        application.addInterview(interview);}
+        catch (ClassCastException e){
+            return  "not matched";
+        }
+        return  "Interview Matched";
     }
+
+    public  ArrayList findJobPostings(String state ){
+        return  JobPostings.get(state);
+    }
+
+    public  void  postJob(String hr ,ArrayList job){
+            JobPosting jobPosting = new JobPosting(job,this);
+            JobPostingManager.addJobPostings(jobPosting);
+            ArrayList lst = JobPostings.get(hr);
+            lst.add(jobPosting);
+            JobPostings.put(hr,lst);// code smell
+    }
+
+
+    public  ArrayList<Interview>findUpcomingInterveiws( String interviewer){
+
+        return  upcomingInterview.get(interviewer);
+
+    }
+
+    public  ArrayList<Interview>WaitingForResultInterviews( String interviewer){
+        return  waitingForResultInterviews.get(interviewer);
+
+    }
+
+
+    public  void  removeInterview(Interview interview){
+        String interviewer = interview.getinterviewer();
+        removeValueInArraylistInHashmap(waitingForResultInterviews,interviewer,interview);
+
+    }
+
+    public  void  moveInterview(Interview interview){
+        String interviewer = interview.getinterviewer();
+        removeValueInArraylistInHashmap(waitingForResultInterviews,interviewer,interview);
+        addValueToArraylistInHashmap(upcomingInterview,interviewer,interview);
+    }
+
+
+
+
+    public ArrayList<Interview> getValuesFromMap(HashMap<String,Interview> map, ArrayList lst){
+
+            for (String key:map.keySet()
+            ) {lst.add(map.get(key));
+
+            }
+
+        return  lst;
+    }
+
+
     public void addInterviewer(String interviewer){
         //need to be done
     }
-    public  void  removeInterviewer(String interviewer){
-        // need to be done
-    }
+
     public void  assignInterviewer(String interviewer){
         //need to be done
     }
@@ -97,21 +144,13 @@ public class Company extends  User{
         //need to be done
     }
 
-    public ArrayList<Interview> findInterviews(String interviewer , Interview.InterviewState state){
-            return  new ArrayList<>();
-            // need to be done
-    }
+
     public ArrayList<String> findInterviewers(JobPosting jobPosting){
         return  new ArrayList<>();
         // need to be done
     }
-    public ArrayList<JobPosting> findJobPosting(String hr, JobPostingState state){
-        return  new ArrayList<>();
-        // need to be done
-    }
-    public  void  postJob(String hr ,ArrayList job){
 
-    }
+
 
     public  void  removePost(String  hr, JobPosting job ){
 
@@ -124,7 +163,7 @@ public class Company extends  User{
 
 
     }
-    public  void addValueToArraylistInHashmap(HashMap<Object,ArrayList> map,Object key, Object value ){
+    public  void addValueToArraylistInHashmap(HashMap<String,ArrayList<Interview>> map,String key, Object value ){
 
         try {ArrayList v = map.get(key);
 
@@ -135,7 +174,20 @@ public class Company extends  User{
         }
 
     }
-    
+    public  void removeValueInArraylistInHashmap(HashMap<String,ArrayList<Interview>> map,String key, Object value ){
+
+        try {ArrayList v = map.get(key);
+
+            v.remove(value);
+            map.put(key,v);}
+        catch (ClassCastException e){
+            System.out.println("try the right type");
+        }
+
+    }
+
+
+
 }
 
 
