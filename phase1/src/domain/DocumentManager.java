f69db28d3ba56f36da7f743a88e0a87ea5cc0d85
Yichun Zhang <yichun.zhang@mail.utoronto.ca>
2019-07-10 11:36:42 -0400

    Update DocumentManager. Remove files FileSystem and TestFileSystem

--- phase1/src/domain/DocumentManager.java
+++ phase1/src/domain/DocumentManager.java
@@ -1,13 +1,12 @@
 package domain;
 
 import java.io.*;
-import java.nio.file.Paths;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
+
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 class DocumentManager {
     private HashMap<LocalDate, ArrayList<Applicant>> deleteAfterThirtyDays;
@@ -24,7 +23,7 @@ class DocumentManager {
         return documentManager;
     }
 
-    private DocumentManager(){
+    private DocumentManager() {
         deleteAfterThirtyDays = new HashMap<>();
         attachedDocuments = new HashMap<>();
         allDocuments = new HashMap<>();
@@ -32,7 +31,7 @@ class DocumentManager {
     }
 
 
-//todo: deal with exceptions in this class
+    //todo: deal with exceptions in this class
     String viewDocument(File file) {
 //        fileName is the full path of the file
         StringBuilder content = new StringBuilder();
@@ -64,15 +63,14 @@ class DocumentManager {
         }
     }
 
-    void createNewDocument(String Filename, String content, String Applicant)throws IOException {
+    void createNewDocument(String Filename, String content, String applicant) throws IOException {
         boolean isExisted = false;
         File file = new File(Filename);
-        if(file.createNewFile()){
+        if (file.createNewFile()) {
             writeDoc(file, content);
             allFiles.put(Filename, file);
-            addToAllDocuments(Applicant, file);
-//todo:            deleteAfterThirtyDays.put(LocalDate.now(), ); //Arraylist<applicant>
-        }else{
+            addToAllDocuments(applicant, file);
+        } else {
             System.out.println("File already existed!");
         }
     }
@@ -80,59 +78,68 @@ class DocumentManager {
     void deleteDocument(String fileName) {
         File file = allFiles.get(fileName);
         allFiles.remove(fileName);
-        if(file.delete())
-        {
+        if (file.delete()) {
             System.out.println("File is deleted successfully");
-        }
-        else
-        {
+        } else {
             System.out.println("Failed to delete the document: " + fileName);
         }
     }
 
     void updateDocument(String fileName, String content) {
         try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(allFiles.get(fileName), false))) {
-            bufferedWriter.write(content);}
-        catch(IOException e){
+            bufferedWriter.write(content);
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     void deleteAllInactiveDocuments() {
-//todo
-    }
-
-    private void addToMap(String key, File file, HashMap<String, ArrayList<File>> map){
-        ArrayList<File> files;
-        if(map.containsKey(key)) {
-            files = map.get(key);
-            files.add(file);
-        }else{
-            files = new ArrayList<>();
-            files.add(file);
-            map.put(key, files);
+        for (LocalDate addedDate : deleteAfterThirtyDays.keySet()) {
+            if (DAYS.between(addedDate, LocalDate.now()) > 30) {
+                deleteAfterThirtyDays.remove(addedDate);
+            }
         }
     }
 
-    private void addToAllDocuments(String Applicant, File file){
+    private void addToMap(String keyToAdd, File newValue, HashMap<String, ArrayList<File>> map) {
+        if (map.containsKey(keyToAdd)) {
+            map.get(keyToAdd).add(newValue);
+        } else {
+            ArrayList<File> values = new ArrayList<>();
+            values.add(newValue);
+            map.put(keyToAdd, values);
+        }
+    }
+
+    private void addToAllDocuments(String Applicant, File file) {
         addToMap(Applicant, file, this.allDocuments);
     }
 
-    void addToAttachedDocuments(String Application, File file){
+    void addToAttachedDocuments(String Application, File file) {
         addToMap(Application, file, this.attachedDocuments);
     }
 
+    void addToDeleteAfterThirtyDays(Applicant applicant) {
+        ArrayList<Applicant> applicants;
+        if (deleteAfterThirtyDays.containsKey(LocalDate.now())) {
+            applicants = deleteAfterThirtyDays.get(LocalDate.now());
+            applicants.add(applicant);
+        } else {
+            applicants = new ArrayList<>();
+            applicants.add(applicant);
+            deleteAfterThirtyDays.put(LocalDate.now(), applicants);
+        }
+    }
 
     void removeFromDeleteAfterThirtyDays(Applicant applicant) {
         ArrayList<Applicant> lst;
-        if(deleteAfterThirtyDays.containsValue(applicant)){
-            for(LocalDate closeDate: deleteAfterThirtyDays.keySet()){
-                if((lst=deleteAfterThirtyDays.get(closeDate)).contains(applicant)){
+        if (deleteAfterThirtyDays.containsValue(applicant)) {
+            for (LocalDate addedDate : deleteAfterThirtyDays.keySet()) {
+                if ((lst = deleteAfterThirtyDays.get(addedDate)).contains(applicant)) {
                     lst.remove(applicant);
                 }
             }
         }
     }
-
 
 }
