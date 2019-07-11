dad1a61af1d13bec200c66d836aabde4d51d3876
Jin Zhang <jinzhang.zhang@mail.utoronto.ca>
2019-07-10 22:17:31 -0400

    Change TheSystem back to Singleton so system can run

--- phase1/src/domain/TheSystem.java
+++ phase1/src/domain/TheSystem.java
@@ -7,9 +7,18 @@ import java.io.Serializable;
 
 public class TheSystem implements Serializable {
 
-    private DocumentManager documentManager = new DocumentManager();
-    private JobPostingManager jobPostingManager = new JobPostingManager();
-    private AccountManager accountManager = new AccountManager();
+    private static TheSystem ourInstance = new TheSystem();
+
+    static DocumentManager documentManager = new DocumentManager();
+    static JobPostingManager jobPostingManager = new JobPostingManager();
+    static AccountManager accountManager = new AccountManager();
+
+    private TheSystem(){}
+
+    public static TheSystem getInstance() {
+        return ourInstance;
+    }
+
 
     public static void main(String[] args) {
         TheSystem system = new TheSystem();
