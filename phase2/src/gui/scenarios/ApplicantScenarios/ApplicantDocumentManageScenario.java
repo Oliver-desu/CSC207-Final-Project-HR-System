6081d0f57b244bc2596a04d6469e50c271cd8504
baibizhe <bbzatyddounan@gmail.com>
2019-07-22 02:34:23 -0400

    five applicant scrnario have been created.finished interviewScenario

---
+++ phase2/src/gui/scenarios/ApplicantScenarios/ApplicantDocumentManageScenario.java
@@ -0,0 +1,16 @@
+package gui.scenarios.ApplicantScenarios;
+
+import gui.major.Scenario;
+import gui.major.UserMenu;
+
+public class ApplicantDocumentManageScenario extends Scenario {
+
+    protected ApplicantDocumentManageScenario(UserMenu userMenu, LayoutMode mode) {
+        super(userMenu, mode);
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
---
+++ phase2/src/gui/scenarios/ApplicantScenarios/SubmitDocumentManageScenario.java
@@ -0,0 +1,17 @@
+package gui.scenarios.ApplicantScenarios;
+
+import gui.major.Scenario;
+import gui.major.UserMenu;
+
+public class SubmitDocumentManageScenario extends Scenario {
+
+
+    protected SubmitDocumentManageScenario(UserMenu userMenu, LayoutMode mode) {
+        super(userMenu, mode);
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
