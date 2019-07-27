d85215cebe1c221be9a02da0c7578f29da767ba2
baibizhe <bbzatyddounan@gmail.com>
2019-07-27 18:31:40 -0400

    finished ApplicationManagerScenario and update JobSearchingScenario

--- phase2/src/gui/scenarios/ApplicationManageScenario.java
+++ phase2/src/gui/scenarios/ApplicationManageScenario.java
@@ -1,8 +1,19 @@
 package gui.scenarios;
 
+import domain.applying.Application;
+import domain.applying.Document;
 import domain.user.Applicant;
 import gui.major.Scenario;
 import gui.major.UserMenu;
+import gui.panels.FilterPanel;
+import gui.scenarios.oliver.DocumentManageScenario;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ApplicationManageScenario  extends Scenario {
     private  Applicant applicant;
@@ -10,10 +21,119 @@ public class ApplicationManageScenario  extends Scenario {
 
         super(userMenu, LayoutMode.REGULAR);
         this.applicant = applicant;
-        System.out.println("credted ApplicationManageScenario");
+        System.out.println("created ApplicationManageScenario");
     }
+
+    public static void main(String[] args) {
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("username", "oliver");
+        Applicant applicant = new Applicant(hashMap);
+        ApplicationManageScenario applicationManageScenario = new ApplicationManageScenario(new UserMenu(), applicant);
+        applicationManageScenario.exampleView();
+    }
+
     public void init(){
         super.init();
-        // # Todo : not finishted
+        initLeftFilter();
+        initNullButtons();
     }
+
+    public void initNullButtons() {
+        NullListener nullListener = new NullListener();
+        addButton("EditApplication", nullListener);
+        addButton("DeleteApplication", nullListener);
+        addButton("Apply", nullListener);
+        addButton("WithDraw", nullListener);
+        addButton("ViewDocument", nullListener);
+
+    }
+
+    public void initLeftFilter() {
+        ArrayList<Object> filterContent = new ArrayList<>(applicant.getApplications());
+        FilterPanel<Object> filterPanel = getFilterPanel(true);
+        filterPanel.setFilterContent(filterContent);
+        setListener(filterPanel);
+    }
+
+    public void setListener(FilterPanel filterPanel) {
+        filterPanel.addSelectionListener(new LeftFilterListener());
+    }
+
+    public void initButtonPanel(Application application, Applicant applicant) {
+        ButtonListener buttonListener = new ButtonListener(application, applicant);
+        addButton("EditApplication", buttonListener);
+        addButton("DeleteApplication", buttonListener);
+        addButton("Apply", buttonListener);
+        addButton("WithDraw", buttonListener);
+        addButton("ViewDocument", buttonListener);
+    }
+
+    public class NullListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // null listener ,just in order to show buttons
+        }
+    }
+
+    public class LeftFilterListener implements ListSelectionListener {
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            FilterPanel<Object> filterPanel = getFilterPanel(true);
+            FilterPanel<Object> rightfilterPanel = getFilterPanel(false);
+            Application application = (Application) filterPanel.getSelectObject();
+            if (application.getStatus() == Application.ApplicationStatus.DRAFT) {
+                initButtonPanel(application, applicant);
+            }
+            // 如果这个application是draft 那我在这里给button添加真正的按钮和listener
+            setOutputText(application.toString());
+            ArrayList<Object> documentArrayList = new ArrayList<>(application.getDocumentManager().getAllDocuments());
+            rightfilterPanel.setFilterContent(documentArrayList);
+            rightfilterPanel.addSelectionListener(new rightFilterListener());
+            //在这选定文件会直接弹出窗口显示文件
+
+        }
+    }
+
+    public class ButtonListener implements ActionListener {
+        private Application application;
+        private Applicant applicant;
+
+        public ButtonListener(Application application, Applicant applicant) {
+            this.application = application;
+            this.applicant = applicant;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            String buttonName = e.getActionCommand();
+            switch (buttonName) {
+                case "ViewDocument":
+                    System.out.println(buttonName);
+                    break;
+                case "EditApplication":
+                    DocumentManageScenario documentManageScenario = new DocumentManageScenario(getUserMenu(), applicant.getDocumentManager(), application.getDocumentManager());
+                    switchScenario(documentManageScenario);
+                    System.out.println("EditApplication");
+                    break;
+                case "Apply":
+                    application.apply(getMain().getJobPool(), getMain().getCompanyPool());
+                    System.out.println("Apply successfully");
+                case "WithDraw":
+                    application.cancel(getMain().getJobPool(), getMain().getCompanyPool());
+                    System.out.println("Cancel successfully");
+            }
+        }
+    }
+
+    public class rightFilterListener implements ListSelectionListener {
+
+        @Override
+        public void valueChanged(ListSelectionEvent e) {
+            FilterPanel<Object> rightfilterPanel = getFilterPanel(false);
+            Document document = (Document) rightfilterPanel.getSelectObject();
+            showDocument(document.toString());
+        }
+    }
+
+
 }
--- phase2/src/gui/scenarios/JobSearchingScenario.java
+++ phase2/src/gui/scenarios/JobSearchingScenario.java
@@ -1,5 +1,6 @@
 package gui.scenarios;
 
+import domain.applying.Application;
 import domain.job.JobPosting;
 import domain.storage.JobPool;
 import domain.user.Applicant;
@@ -32,41 +33,50 @@ public class JobSearchingScenario extends  Scenario{
         leftFilterPanel.setFilterContent(lst);
         setListner(leftFilterPanel);
         leftFilterPanel.updateUI();
-        addButton("CreatApplication",new JumptoApplicationManageScenarioButton());
-       // ArrayList<String >AbbreviationOfJobPosting = AbbreviationOfJobPosting(jobPostings);
-
     }
-    class JumptoApplicationManageScenarioButton implements ActionListener{
 
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String buttonName = e.getActionCommand();
-            ApplicationManageScenario appm=  new ApplicationManageScenario(getUserMenu(),applicant);
-            NullScenario aa = new NullScenario(getUserMenu());
-            switchScenario(appm);
 
-            // Todo:
-            System.out.println("botton"); //used for test if successful hitted botton
-
-        }
-    }
 
     public  void setListner(FilterPanel panel){
         panel.addSelectionListener(new ListSelectionListener() {
             @Override
             public void valueChanged(ListSelectionEvent e) {
-                try {JobPosting object = (JobPosting) panel.getSelectObject();
+                try {
+                    JobPosting object = (JobPosting) panel.getSelectObject(); //selected JobPosting
                     setOutputText(object.toString());
+                    setButtonListener(object); //then pass this jobPosting to listener
+
                 }
                 catch (ClassCastException a){
-                    System.out.println("what you given is not a Jobostingg ing ing ");
+                    System.out.println("what you given is not a JobPosting ");
                 }
-
             }
         });
 
     }
 
+    public void setButtonListener(JobPosting jobPosting) {
+        addButton("CreatApplication", new JumptoApplicationManageScenarioButton(jobPosting));
+
+    }
+
+    class JumptoApplicationManageScenarioButton implements ActionListener {
+        public JobPosting jobPosting;
+
+        public JumptoApplicationManageScenarioButton(JobPosting jobPosting) {
+            this.jobPosting = jobPosting;
+        }
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Application application = new Application();
+            applicant.addApplication(jobPosting.getJobId(), application);
+            ApplicationManageScenario applicationManageScenario = new ApplicationManageScenario(getUserMenu(), applicant);
+            switchScenario(applicationManageScenario);
+
+
+        }
+    }
     public static void main(String[] args) {
         HashMap<String, String> hashMap = new HashMap<>();
         hashMap.put("username", "oliver");
