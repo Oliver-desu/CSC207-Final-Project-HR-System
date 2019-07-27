b25f9a60eb3199c0531cb33e2e3a3bddd7909c13
Sharon <fangchu.lin@mail.utoronto.ca>
2019-07-27 02:38:58 -0400

    Created HRGeneralistJobSearchingScenario

---
+++ phase2/src/gui/scenarios/HRGeneralistJobSearchingScenario.java
@@ -0,0 +1,61 @@
+package gui.scenarios;
+
+import domain.job.JobPosting;
+import domain.storage.JobPool;
+import domain.user.HRGeneralist;
+import gui.major.Scenario;
+import gui.major.UserMenu;
+import gui.panels.FilterPanel;
+
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+public class HRGeneralistJobSearchingScenario extends Scenario {
+
+    private ArrayList<JobPosting> jobPostings = JobPool.getJobPostings();
+    private HRGeneralist hrGeneralist;
+
+    public HRGeneralistJobSearchingScenario(UserMenu userMenu, HRGeneralist hrGeneralist){
+        super(userMenu, Scenario.LayoutMode.REGULAR);
+        this.hrGeneralist = hrGeneralist;
+    }
+
+    public void init(){
+        super.init();
+        FilterPanel<Object> leftFilterPanel = getFilterPanel(true);
+        ArrayList<Object> jobs = new ArrayList<>();
+        jobs.addAll(jobPostings);
+        leftFilterPanel.setFilterContent(lst);
+        setListner(leftFilterPanel);
+        leftFilterPanel.updateUI();
+
+    }
+
+    public void setListener(FilterPanel panel){
+        panel.addSelectionListener(new ListSelectionListener()){
+            public void valueChanged(ListSelectionEvent e){
+                try {
+                    JobPosting object = (JobPosting) panel.getSelectObject();
+                    setOutputText(object.toString());
+                }
+                catch (ClassCastException a ){
+                    System.out.println("What you given is not JobPosting!!");
+                }
+
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        HashMap<String, String> hashMap = new HashMap<>();
+        hashMap.put("username", "sharon");
+        HRGeneralist hrGeneralist = new HRGeneralist(hashMap, "111");
+        HRGeneralistJobSearchingScenario hrGeneralistJobSearchingScenario = new HRGeneralistJobSearchingScenario(new UserMenu(), hrGeneralist);
+        HRGeneralistJobSearchingScenario.exampleView();
+
+    }
+
+}
