aeb630a12c92a64e4f9d4c8de0547f846c264c4d
Kerwyny <40316624+Kerwyny@users.noreply.github.com>
2019-08-05 17:41:43 -0400

    Finish documentation of Enums

--- phase2/src/domain/Enums/InterviewRoundStatus.java
+++ phase2/src/domain/Enums/InterviewRoundStatus.java
@@ -1,21 +1,36 @@
 package domain.Enums;
 
-import domain.job.InterviewRound;
-import domain.job.InterviewRoundManager;
-
 /**
- * Class {@code InterviewRoundStatus} storage the different status of InterviewRound
- * @see  domain.job.InterviewRound#InterviewRound(String)
- * @see  InterviewRound#checkStatus()
- * @see  InterviewRoundManager#getCurrentInterviewRound()
- * @see  InterviewRoundManager#nextRound()
+ * Enum {@code InterviewRoundStatus} contains different status of {@code InterviewRound}.
+ *
  * @author group 0120 of CSC207 summer 2019
+ * @see  domain.job.InterviewRound
+ * @since 2019-08-04
  */
 public enum InterviewRoundStatus {
 
+    /**
+     * {@code EMPTY} means the {@code InterviewRound} has been initialized but
+     * there is nothing in it, and is still waiting to begin.
+     */
     EMPTY,
+
+    /**
+     * {@code MATCHING} means all the interviews in the {@code InterviewRound}
+     * are either being matched or have been matched by recruiter.
+     */
     MATCHING,
+
+    /**
+     * {@code PENDING} means all interviews have been arranged and is waiting
+     * for the results from interviewers.
+     */
     PENDING,
+
+    /**
+     * {@code FINISHED} means the current {@code InterviewRound} has been
+     * finished.
+     */
     FINISHED
 
 }
