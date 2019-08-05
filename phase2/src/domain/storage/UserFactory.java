e55e3dfde9d2821f4df8f76abefc0c3decedade9
Yichun Zhang <yichun.zhang@mail.utoronto.ca>
2019-08-05 17:11:09 -0400

    Finish documentation of UserFactory.

--- phase2/src/domain/storage/UserFactory.java
+++ phase2/src/domain/storage/UserFactory.java
@@ -5,6 +5,7 @@ import domain.Exceptions.UnmatchedPasswordException;
 import domain.Exceptions.UserAlreadyExistsException;
 import domain.Exceptions.WrongEmailFormatException;
 import domain.user.*;
+import gui.major.UserRegister;
 
 import java.io.Serializable;
 import java.util.HashMap;
@@ -32,10 +33,24 @@ public class UserFactory implements Serializable {
     private Storage Storage;
 
 
+    /**
+     * Create a new user factory.
+     *
+     * @param userPool the {@code Storage} used to store all users created
+     * @see gui.major.UserRegister
+     */
     public UserFactory(Storage userPool) {
         this.Storage = userPool;
     }
 
+    /**
+     * Create and return a new user given his/her basic information and user type.
+     *
+     * @param infoMap      a hash map that stores all information needed to create a new user
+     * @param registerType the user type of the new user being created
+     * @return a new {@code User}
+     * @see UserRegister
+     */
     public User createUser(HashMap<String, String> infoMap, UserType registerType) {
         validValues(infoMap, registerType);
         User user;
@@ -51,10 +66,24 @@ public class UserFactory implements Serializable {
         return user;
     }
 
+    /**
+     * Create a new applicant.
+     *
+     * @param infoMap the map storing basic information of the applicant
+     * @return the new {@code Applicant}
+     * @see UserFactory#createUser(HashMap, UserType)
+     */
     private User createApplicant(HashMap<String, String> infoMap) {
         return new Applicant(infoMap);
     }
 
+    /**
+     * Create a new recruiter.
+     *
+     * @param infoMap the map storing basic information of the recruiter
+     * @return the new {@code Employee} of type {@code UserType.RECRUITER}
+     * @see UserFactory#createUser(HashMap, UserType)
+     */
     private User createRecruiter(HashMap<String, String> infoMap) {
         String companyId = infoMap.get("Company id:");
         if (companyExists(companyId)) {
@@ -66,6 +95,13 @@ public class UserFactory implements Serializable {
         }
     }
 
+    /**
+     * Create a new Interviewer.
+     *
+     * @param infoMap the map that contains basic information of the interviewer
+     * @return the new {@code Employee} of type {@code UserType.INTERVIEWER}
+     * @see UserFactory#createUser(HashMap, UserType)
+     */
     private User createInterviewer(HashMap<String, String> infoMap) {
         String companyId = infoMap.get("Company id:");
         if (companyExists(companyId)) {
@@ -77,6 +113,13 @@ public class UserFactory implements Serializable {
         }
     }
 
+    /**
+     * Create a new company and its hiring manager.
+     *
+     * @param infoMap the map that contains the basic information of the new company and its hiring manager
+     * @return the new {@code Employee} of type {@code UserType.HIRING_MANAGER}
+     * @see UserFactory#createUser(HashMap, UserType)
+     */
     private User createHiringManagerAndCompany(HashMap<String, String> infoMap) {
         String companyId = infoMap.get("Company id:");
         if (!companyExists(companyId)) {
@@ -90,10 +133,28 @@ public class UserFactory implements Serializable {
         }
     }
 
+    /**
+     * Return whether a company exists.
+     * A helper method for createRecruiter, createInterviewer, createHiringManagerAndCompany.
+     *
+     * @param companyId the id of the company to be checked
+     * @return whether the company exists
+     * @see UserFactory#createRecruiter(HashMap)
+     * @see UserFactory#createInterviewer(HashMap)
+     * @see UserFactory#createHiringManagerAndCompany(HashMap)
+     */
     private boolean companyExists(String companyId) {
         return Storage.getCompany(companyId) != null;
     }
 
+    /**
+     * Check whether the information in {@code InfoMap} is valid to create a new user.
+     * It is a helper method for createUser.
+     *
+     * @param infoMap      the map that consists of basic information about the user
+     * @param registerType the type of the new user
+     * @see UserFactory#createUser(HashMap, UserType)
+     */
     private void validValues(HashMap<String, String> infoMap, UserType registerType) {
         if (infoMap.get("Password:").equals("[]")) {
             throw new UnmatchedPasswordException();
@@ -103,6 +164,4 @@ public class UserFactory implements Serializable {
             throw new UserAlreadyExistsException();
         }
     }
-
-
 }
