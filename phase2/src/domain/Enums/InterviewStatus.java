6a402ee9ec12f21f407430fd5bde113fd2d5858f
baibizhe <bbzatyddounan@gmail.com>
2019-08-05 15:51:17 -0400

    update documentation of Enums @see parts

--- phase2/src/domain/Enums/InterviewStatus.java
+++ phase2/src/domain/Enums/InterviewStatus.java
@@ -1,8 +1,18 @@
 package domain.Enums;
 
+import domain.applying.Interview;
+import domain.job.InterviewRound;
+import domain.user.Employee;
+
 /**
  * Class {@code InterviewStatus} storage the different status of Interview
- *
+ * @see domain.applying.Application#update(Interview)
+ * @see Interview#match(Employee)
+ * @see Interview#setResult(boolean)
+ * @see  Interview#cancel()
+ * @see  Interview#toString()
+ * @see InterviewRound#getUnmatchedApplications()
+ * @see  InterviewRound#checkStatus()
  * @author group 0120 of CSC207 summer 2019
  */
 
