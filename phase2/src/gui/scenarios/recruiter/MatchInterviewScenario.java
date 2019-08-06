b11b51cee5a668d7e2f19dfe4f229a7d3a04d6c9
sharon <fangchu.lin@mail.utoronto.ca>
2019-08-06 04:27:11 -0400

    Added documentation for MatchInterviewScenario

--- phase2/src/gui/scenarios/recruiter/MatchInterviewScenario.java
+++ phase2/src/gui/scenarios/recruiter/MatchInterviewScenario.java
@@ -19,12 +19,47 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+/**
+ *  Class {@code MatchInterviewScenario} handles the situation of matching interview.
+ *
+ * @author group 0120 of CSC207 summer 2019
+ * @see gui.major.MenuPanel
+ * @since 2019-08-05
+ */
 public class MatchInterviewScenario extends Scenario {
 
+    /**
+     * An {@code InterviewRound}
+     *
+     * @see #update()
+     * @see MatchListener
+     */
     private InterviewRound interviewRound;
+
+    /**
+     * The {@code LeftFilterPanel} in this scenario.
+     *
+     * @see #initComponents()
+     * @see #initLeftFilter()
+     * @see #update()
+     * @see MatchListener
+     */
     private FilterPanel<Application> leftFilter;
+
+    /**
+     * The {@code RightFilterPanel} in this scenario.
+     *
+     * @see #update()
+     * @see #initRightFilter()
+     * @see MatchListener
+     */
     private FilterPanel<Employee> rightFilter;
 
+    /**
+     * Create a new {@code MatchInterviewScenario} that is a {@code Scenario} with title "Match Interview"
+     * @param userMenu the {@code userMenu} that sets up the gui framework
+     * @param interviewRound Interview Rounds being matched
+     */
     public MatchInterviewScenario(UserMenu userMenu, InterviewRound interviewRound) {
         super(userMenu, "Match Interview");
         this.interviewRound = interviewRound;
@@ -45,6 +80,9 @@ public class MatchInterviewScenario extends Scenario {
         new MatchInterviewScenario(new UserMenu(test.getMain(), recruiter), interviewRound).exampleView();
     }
 
+    /**
+     * Override {@code initComponents} in abstract class {@code Scenario}.
+     */
     @Override
     protected void initComponents() {
         initLeftFilter();
@@ -53,16 +91,29 @@ public class MatchInterviewScenario extends Scenario {
         initButton();
     }
 
+    /**
+     * A helper method for {@code initComponents()} that initializes and add the new {@code ButtonPanel}
+     */
     protected void initButton() {
         ButtonPanel buttonPanel = new ButtonPanel(BUTTON_PANEL_SIZE);
         buttonPanel.addButton("Match", new MatchListener());
         add(buttonPanel);
     }
 
+    /**
+     * A helper method for {@code initComponents()} that initializes {@code leftFilter}.
+     *
+     * @see #initComponents()
+     */
+
     protected void initLeftFilter() {
         leftFilter = new FilterPanel<>(LIST_SIZE, "Unmatched Applications");
         add(leftFilter);
     }
+
+    /**
+     * Override {@code update()} in abstract class {@code Scenario}.
+     */
 
     @Override
     protected void update() {
@@ -73,11 +124,23 @@ public class MatchInterviewScenario extends Scenario {
         rightFilter.setFilterContent(interviewers);
     }
 
+    /**
+     * A helper method for {@code initComponents()} that initializes {@code rightFilter}.
+     *
+     * @see #initComponents()
+     */
     protected void initRightFilter() {
         rightFilter = new FilterPanel<>(LIST_SIZE, "Company Interviewers");
         add(rightFilter);
     }
 
+    /**
+     * Class{@code MatchListener} implements ActionListener. It deals with actions related to matching interviews.
+     *
+     * @author group 0120 of CSC207 summer 2019
+     * @see #initLeftFilter()
+     * @since 2019-08-05
+     */
     class MatchListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
