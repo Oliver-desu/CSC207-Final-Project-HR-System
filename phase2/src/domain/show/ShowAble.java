6adb2a879afe892e77c4517d79dbfcde13feec7f
Kerwyny <40316624+Kerwyny@users.noreply.github.com>
2019-08-06 10:44:36 -0400

    Finish documentation for Interfaces

--- phase2/src/domain/show/ShowAble.java
+++ phase2/src/domain/show/ShowAble.java
@@ -1,8 +1,23 @@
 package domain.show;
 
+/**
+ * Classes implement interface {@code ShowAble} will show information to
+ * users in GUI.
+ *
+ * @author group 0120 of CSC207 summer 2019
+ * @since 2019-08-06
+ */
 public interface ShowAble {
+
     String toString();
 
+    /**
+     * Helper method for {@code toString} method.
+     *
+     * @param header      header
+     * @param description value for each header
+     * @return a structured line of header and value pair
+     */
     default String getInfoString(String header, String description) {
         return header + ": " + description + "\n";
     }
