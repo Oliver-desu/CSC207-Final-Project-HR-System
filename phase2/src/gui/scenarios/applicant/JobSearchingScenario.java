81cac4c723aca95eb51ca5851d287a11fff92f00
baibizhe <bbzatyddounan@gmail.com>
2019-08-06 03:21:20 -0400

    update JobSearchingScenario and  ViewInterviewScenario

--- phase2/src/gui/scenarios/applicant/JobSearchingScenario.java
+++ phase2/src/gui/scenarios/applicant/JobSearchingScenario.java
@@ -14,12 +14,25 @@ import gui.panels.FilterPanel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * Class {@code  JobSearchingScenario} the page used for Job searching
+ *
+ * @see gui.major.MenuPanel
+ * @since 2019-08-06
+ */
 public class JobSearchingScenario extends Scenario {
+    /**
+     * the {@code FilterPanel} on the left stored the {@code JobPosting}
+     */
+    private FilterPanel<JobPosting> leftFilter;
+
+    /**
+     * constructor for JobSearchingScenario,
+     * @param userMenu given {@code UserMenu}
+     */
     public JobSearchingScenario(UserMenu userMenu) {
         super(userMenu, "Job Searching");
     }
-
-    private FilterPanel<JobPosting> leftFilter;
 
     public static void main(String[] args) {
         Test test = new Test();
@@ -37,6 +50,10 @@ public class JobSearchingScenario extends Scenario {
         initButton();
     }
 
+    /**
+     * override the method in parent class  {@code scenario}
+     *update the content on the leftFilterPanel  by get the latest version of JobPosting from {@code Storage}
+     */
     @Override
     protected void update() {
         Storage Storage = getMain().getStorage();
@@ -49,13 +66,30 @@ public class JobSearchingScenario extends Scenario {
         add(leftFilter);
     }
 
+    /**
+     * Initial a button  called "Create Application" with  new {@code CreateApplicationListener},
+     * then add this button to the ButtonPanel
+     */
     protected void initButton() {
         ButtonPanel buttonPanel = new ButtonPanel(BUTTON_PANEL_SIZE);
         buttonPanel.addButton("Create Application", new CreateApplicationListener());
         add(buttonPanel);
     }
 
+    /**
+     * Class {@code  CreateApplicationListener} listener used for creat a new application
+     * @see #initButton()
+     * @since 2019-08-06
+     */
     class CreateApplicationListener implements ActionListener {
+        /**
+         * override the method in interface {@code ActionListener}
+         * 1 get the {@code JobPosting} selected by user .2 get the {@code Applicant } stored in the
+         * {@code UserMenu} 3  if the {@code JobPosing} is not null the add it to the {@code Applicant}
+         * then switch to the {@code ApplicationManageScenario}
+         * otherwise show a massage "Failed!".
+         * @param e ActionEvent
+         */
         @Override
         public void actionPerformed(ActionEvent e) {
             JobPosting jobPosting = leftFilter.getSelectObject();
--- phase2/src/gui/scenarios/applicant/ViewInterviewScenario.java
+++ phase2/src/gui/scenarios/applicant/ViewInterviewScenario.java
@@ -9,11 +9,26 @@ import gui.major.Scenario;
 import gui.major.UserMenu;
 import gui.panels.FilterPanel;
 
+/**
+ * Class {@code  ViewInterviewScenario} viewing all interviews scenario
+ *
+ * @see gui.major.MenuPanel
+ * @since 2019-08-06
+ */
 public class ViewInterviewScenario extends Scenario {
-
+    /**
+     * the {@code FilterPanel} on the left stored the {@code Interview}
+     */
     private FilterPanel<Interview> leftFilter;
+    /**
+     * the {@code FilterPanel} in the middle  stored the {@code Interview}
+     */
     private FilterPanel<Interview> rightFilter;
 
+    /**
+     * constructor for a new {@code ViewInterviewScenario}
+     * @param userMenu given {@code UserMenu}
+     */
     public ViewInterviewScenario(UserMenu userMenu) {
         super(userMenu, "View Interview");
     }
@@ -32,6 +47,10 @@ public class ViewInterviewScenario extends Scenario {
 
     }
 
+    /**
+     * override the method in parent class  {@code Scenario}
+     * initial the leftFilter , right Filter and ButtonPanel.
+     */
     @Override
     protected void initComponents() {
         initLeftFilter();
@@ -39,6 +58,11 @@ public class ViewInterviewScenario extends Scenario {
         initOutputInfoPanel();
     }
 
+    /**
+     * override the method in parent class  {@code Scenario}
+     * set the content in the leftFilterPanel  to be the OngoingInterviews of the {@code Applicant}
+     * set the content in the rightFilterPanel  to be the PastInterviews of the {@code Applicant}
+     */
     @Override
     protected void update() {
         // Todo: need left filter be ongoing interviews, right filter be past interviews
